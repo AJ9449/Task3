0f955f95792e3e092c1000075144881ee2220ecd
Abirami <abirami.jayabal7@gmail.com>
2023-09-10 12:56:56 +0800

    Comit Task 3

---
+++ json_for.js
@@ -0,0 +1,34 @@
+const jsonData = {
+    "people": [
+        { "name": "User1", "age": 11 },
+        { "name": "User2", "age": 22 },
+        { "name": "User3", "age": 33 }
+    ]
+};
+
+
+console.log("For Loop")
+{
+    for(var i=0; i< jsonData.people.length; i++)
+    {
+        var data= jsonData.people[i];
+        console.log(`Name; ${data.name}, Age: ${data.age}`);
+    }
+}
+
+console.log("ForEach")
+{
+    jsonData.people.forEach(data)
+    {
+        console.log(`Name: ${data.name} Age: ${data.age}`);
+    }
+}
+
+
+console.log("For...of");
+{
+    for(var data of jsonData.people)
+    {
+        console.log(`Name: ${data.name}, Age: ${data.age}`);
+    }
+}
